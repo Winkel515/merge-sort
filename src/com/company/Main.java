c77fed517f38e92bebbc69dc23d24160e48f8e77
Winkel515 <winkel515@hotmail.com>
2020-06-09 13:44:52 -0400

    implemented merge sort that mutates array

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,49 +5,69 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        int[] arr = new int[]{1,5,3,6,2,6,2,18,34,3};
-        arr = mergeSort(arr);
+        int[] arr = new int[]{96,5,255,6,39,66,256,18,34,3};
+        mergeSort(arr,0, arr.length);
         System.out.println(Arrays.toString(arr));
     }
 
-    public static int[] mergeSort(int[] arr) {
-        int middle = arr.length/2;
-        int[] arr1 = new int[middle];
-        int[] arr2 = new int[arr.length - middle];
-        for(int i = 0; i < middle; i++) {
-            arr1[i] = arr[i];
+    public static void mergeSort(int[] arr, int begin, int end) {
+        int middle = (end+begin)/2;
+        if(middle - begin > 1) {
+            mergeSort(arr, begin, middle);
         }
-        for(int i = middle, j = 0; i < arr.length; i++, j++) {
-            arr2[j] = arr[i];
+        if(end - middle > 1) {
+            mergeSort(arr, middle, end);
         }
-        if(arr1.length > 1) {
-            arr1 = mergeSort(arr1);
-        }
-        if(arr2.length > 1) {
-            arr2 = mergeSort(arr2);
-        }
-        return merge(arr1, arr2);
+        merge(arr, begin, middle, end);
     }
 
-    public static int[] merge(int[] arr1, int[] arr2) {
-        int[] merged = new int[arr1.length + arr2.length];
+    public static void merge(int[]arr, int begin, int middle, int end) {
+        int[] arr1 = new int[middle-begin];
+        int[] arr2 = new int[end-middle];
+        for(int i = 0; i < arr1.length; i++) {
+            arr1[i] = arr[begin+i];
+        }
+        for(int i = 0; i < arr2.length; i++) {
+            arr2[i] = arr[middle+i];
+        }
         int a = 0;
         int b = 0;
-        for(int i = 0; i < merged.length; i++) {
+        for(int i = begin; i < end; i++) {
             if(a == arr1.length) {
-                merged[i] = arr2[b];
+                arr[i] = arr2[b];
                 b++;
-            } else if (b == arr2.length) {
-                merged[i] = arr1[a];
+            } else if(b == arr2.length) {
+                arr[i] = arr1[a];
                 a++;
-            } else if(arr1[a] < arr2[b]) {
-                merged[i] = arr1[a];
+            } else if (arr1[a] < arr2[b]) {
+                arr[i] = arr1[a];
                 a++;
             } else {
-                merged[i] = arr2[b];
+                arr[i] = arr2[b];
                 b++;
             }
         }
-        return merged;
     }
+
+//    public static int[] merge(int[] arr1, int[] arr2) {
+//        int[] merged = new int[arr1.length + arr2.length];
+//        int a = 0;
+//        int b = 0;
+//        for(int i = 0; i < merged.length; i++) {
+//            if(a == arr1.length) {
+//                merged[i] = arr2[b];
+//                b++;
+//            } else if (b == arr2.length) {
+//                merged[i] = arr1[a];
+//                a++;
+//            } else if(arr1[a] < arr2[b]) {
+//                merged[i] = arr1[a];
+//                a++;
+//            } else {
+//                merged[i] = arr2[b];
+//                b++;
+//            }
+//        }
+//        return merged;
+//    }
 }
